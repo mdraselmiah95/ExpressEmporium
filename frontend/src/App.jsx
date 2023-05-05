e3e6f84965d071979a566729c5ea01f30c4d5370
mdraselmiah95 <russelroy15@gmail.com>
2023-05-05 12:14:23 +0600

    ExpressEmporium MERN Project Frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,8 @@
-import "./App.css";
-
 function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline text-green-500">
-        Hello world!
+        Hello world! Rasel
       </h1>
     </>
   );
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      Roboto: ["Roboto", "sans-serif"],
+      Poppins: ["Poppins", "sans-serif"],
+    },
+    extend: {
+      screens: {
+        "1000px": "1050px",
+        "1100px": "1110px",
+        "800px": "800px",
+        "1300px": "1300px",
+        "400px": "400px",
+      },
+    },
   },
   plugins: [],
 };
