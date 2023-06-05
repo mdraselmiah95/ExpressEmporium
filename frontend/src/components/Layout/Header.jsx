129265278d74060489c392a6fda6e7ea894f2d42
mdraselmiah95 <russelroy15@gmail.com>
2023-06-05 23:52:01 +0600

    WIP on Shop fronten

--- frontend/src/components/Layout/Header.jsx
+++ frontend/src/components/Layout/Header.jsx
@@ -1,7 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import {
+  AiOutlineHeart,
+  AiOutlineSearch,
+  AiOutlineShoppingCart,
+} from "react-icons/ai";
+import styles from "../../styles/styles";
+import { productData } from "../../static/data";
 
 const Header = () => {
-  return <div>Header</div>;
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchData, setSearchData] = useState(null);
+
+  const handleSearchChange = (e) => {
+    const term = e.target.value;
+    setSearchTerm(term);
+
+    const filteredProducts =
+      productData &&
+      productData.filter((product) =>
+        product.name.toLowerCase().includes(term.toLowerCase())
+      );
+    setSearchData(filteredProducts);
+  };
+  return (
+    <div className={`${styles.section}`}>
+      <div className="hidden 800px:h-[50px] 800px:my-[20px] 800px:flex items-center justify-between">
+        <div>
+          <Link to="/">
+            <img
+              src="https://shopo.quomodothemes.website/assets/images/logo.svg"
+              alt="logo"
+            />
+          </Link>
+        </div>
+        {/* search box */}
+        <div className="w-[50%] relative">
+          <input
+            type="text"
+            placeholder="Search Product..."
+            value={searchTerm}
+            onChange={handleSearchChange}
+            className="h-[40px] w-full px-2 border-[#3957db] border-[2px] rounded-md"
+          />
+          <AiOutlineSearch
+            size={30}
+            className="absolute right-2 top-1.5 cursor-pointer"
+          />
+          {searchData && searchData.length !== 0 ? (
+            <div className="absolute min-h-[30vh] bg-slate-50 shadow-sm-2 z-[9] p-4">
+              {searchData &&
+                searchData.map((i, index) => {
+                  const d = i.name;
+                  const product_name = d.replace(/\s+/g, "-");
+                  return (
+                    <Link to={`/product/${product_name}`} key={index}>
+                      <div className="w-full flex items-start-py-3">
+                        <img
+                          src={i.image_Url[0].url}
+                          alt="product image"
+                          className="w-[40px] h-[40px] mr-[10px]"
+                        />
+                        <h1>{i.name}</h1>
+                      </div>
+                    </Link>
+                  );
+                })}
+            </div>
+          ) : null}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
